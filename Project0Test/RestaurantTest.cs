637c1a536c371b58bf47cc9c314f1b3b13b8c4df
Kay Choi <kay.n.choi@gmail.com>
2018-04-20 13:57:54 -0700

    Initial project architecture setup.

---
+++ Project0Test/RestaurantTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Project0Test
+{
+    [TestClass]
+    public class RestaurantTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
