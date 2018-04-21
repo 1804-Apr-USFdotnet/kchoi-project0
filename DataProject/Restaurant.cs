83480a801438ae4104e975ed792ddf9a8c542e04
Kay Choi <kay.n.choi@gmail.com>
2018-04-21 10:36:25 -0700

    Added initial properties to Restaurant class.

--- DataProject/Restaurant.cs
+++ DataProject/Restaurant.cs
@@ -8,5 +8,11 @@ namespace DataProject
 {
     public class Restaurant
     {
+        public string Address { get; set; }
+        public List<Review> Reviews { get; set; }
+        public string PhoneNum { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public long ZIP { get; set; }
     }
 }
--- Project0Test/RestaurantTest.cs
+++ Project0Test/RestaurantTest.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using DataProject;
 
 namespace Project0Test
 {
@@ -7,8 +10,67 @@ namespace Project0Test
     public class RestaurantTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestRestaurantAddress()
         {
+            Restaurant res = new Restaurant();
+            string address = "1600 Pennsylvania Ave";
+            res.Address = address;
+            Assert.AreEqual(res.Address, address);
+        }
+
+        [TestMethod]
+        public void TestRestaurantCity()
+        {
+            Restaurant res = new Restaurant();
+            string city = "Washington";
+            res.City = city;
+            Assert.AreEqual(city, res.City);
+        }
+
+        [TestMethod]
+        public void TestRestaurantState()
+        {
+            Restaurant res = new Restaurant();
+            string state = "DC";
+            res.State = state;
+            Assert.AreEqual(res.State, state);
+        }
+
+        [TestMethod]
+        public void TestRestaurantZIP()
+        {
+            Restaurant res = new Restaurant();
+            long zip = 20500;
+            res.ZIP = zip;
+            Assert.AreEqual(zip, res.ZIP);
+        }
+
+        [TestMethod]
+        public void TestRestaurantReviews()
+        {
+            Restaurant res = new Restaurant
+            {
+                Reviews = new List<Review>()
+            };
+            int rating = 5;
+            Review rev = new Review
+            {
+                Rating = rating,
+                Description = "this is a fake description"
+            };
+
+            res.Reviews.Add(rev);
+
+            Assert.IsTrue(res.Reviews.Count == 1 && res.Reviews[0].Rating == rating);
+        }
+
+        [TestMethod]
+        public void TestRestaurantPhoneNum()
+        {
+            Restaurant res = new Restaurant();
+            string phoneNum = "555-555-5555";
+            res.PhoneNum = phoneNum;
+            Assert.AreEqual(phoneNum, res.PhoneNum);
         }
     }
 }
