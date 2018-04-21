d4e3132ec652fea77a627e179a1baf7321ea16a8
Kay Choi <kay.n.choi@gmail.com>
2018-04-21 15:10:21 -0700

    Added Reviewer class.

---
+++ Project0Test/ReviewerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using LibraryProject;
+
+namespace Project0Test
+{
+    [TestClass]
+    public class ReviewerTest
+    {
+        Reviewer reviewer;
+        const string name = "John Doe";
+        const int id = 0;
+
+        [TestMethod]
+        public void TestReviewerName()
+        {
+            reviewer = new Reviewer();
+            reviewer.Name = name;
+            Assert.AreEqual(reviewer.Name, name);
+        }
+
+        [TestMethod]
+        public void TestReviewerID()
+        {
+            reviewer = new Reviewer()
+            {
+                ID = id
+            };
+            Assert.AreEqual(reviewer.ID, id);
+        }
+    }
+}
