353e82ceadd1252b956d22a1d7129ae5e68a8264
Kay Choi <kay.n.choi@gmail.com>
2018-04-21 15:09:27 -0700

    Added ITaggable, IReviewable interfaces for Restaurant class.

--- LibraryProject/Restaurant.cs
+++ LibraryProject/Restaurant.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using LibraryProject.Interfaces;
+
 namespace LibraryProject
 {
-    public class Restaurant
+    public class Restaurant : ITaggable, IReviewable
     {
         public string Address { get; set; }
         public List<Review> Reviews { get; set; }
@@ -14,10 +16,12 @@ namespace LibraryProject
         public string City { get; set; }
         public string State { get; set; }
         public long ZIP { get; set; }
+        public List<string> Tags { get; set; }
 
         public Restaurant()
         {
             Reviews = new List<Review>();
+            Tags = new List<string>();
         }
 
         public bool AddReview(Review NewReview)
@@ -25,6 +29,13 @@ namespace LibraryProject
             int count = Reviews.Count;
             Reviews.Add(NewReview);
             return Reviews.Count == count + 1;
+        }
+
+        public bool AddTag(string NewTag)
+        {
+            int count = Tags.Count;
+            Tags.Add(NewTag);
+            return Tags.Count == count + 1;
         }
     }
 }
