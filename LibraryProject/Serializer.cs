449a002fa98e9eb6b4c8fc2eaedc7408bae88e25
Kay Choi <kay.n.choi@gmail.com>
2018-04-23 16:54:07 -0700

    Added Serializer class.

---
+++ LibraryProject/Serializer.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject
+{
+    public static class Serializer
+    {
+        public static void Serialize(string fileName, params object[] obj)
+        {
+            string json;
+
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(fileName, true))
+            {
+                foreach (var i in obj)
+                {
+                    json = JsonConvert.SerializeObject(obj);
+                    file.WriteLine(json);
+                }
+            }
+
+        }
+
+        public static List<T> Deserialize<T>(string fileName)
+        {
+            string json = System.IO.File.ReadAllText(fileName);
+            return JsonConvert.DeserializeObject<List<T>>(json);
+        }
+    }
+}
---
+++ Project0Test/SerializeTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using LibraryProject;
+
+namespace Project0Test
+{
+    [TestClass]
+    public class SerializeTest
+    {
+        [TestMethod]
+        public void TestDeserializeRestaurants()
+        {
+            string filePath = ConfigurationManager.AppSettings["DataDirectory"] + ConfigurationManager.AppSettings["RestaurantsFile"];
+            List<Restaurant> list = Serializer.Deserialize<Restaurant>(filePath);
+            Assert.IsTrue(list.Count > 0);
+        }
+
+        [TestMethod]
+        public void TestDeserializeReviews()
+        {
+            string filePath = ConfigurationManager.AppSettings["DataDirectory"] + ConfigurationManager.AppSettings["ReviewsFile"];
+            List<Review> list = Serializer.Deserialize<Review>(filePath);
+            Assert.IsTrue(list.Count > 0);
+        }
+
+        [TestMethod]
+        public void TestSerializeRestaurants()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestSerializeReviews()
+        {
+            Assert.Fail();
+        }
+    }
+}
