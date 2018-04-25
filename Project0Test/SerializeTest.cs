6ddcd7b0305e78b056d18c86ca015f252073b375
Kay Choi <kay.n.choi@gmail.com>
2018-04-24 21:32:33 -0700

    Deimplemented Review serialization.

--- Project0Test/SerializeTest.cs
+++ Project0Test/SerializeTest.cs
@@ -30,32 +30,7 @@ namespace Project0Test
         }
 
         [TestMethod]
-        public void TestDeserializeReviews()
-        {
-            string filePath = ConfigurationManager.AppSettings["DataDirectory"] + ConfigurationManager.AppSettings["ReviewsFile"];
-            int numEntries = 0;
-            using (System.IO.StreamReader reader = new System.IO.StreamReader(filePath))
-            {
-                while (!reader.EndOfStream)
-                {
-                    reader.ReadLine();
-                    numEntries++;
-                }
-            }
-
-            List<Review> list = Serializer.DeserializeList<Review>(filePath);
-
-            Assert.AreEqual(list.Count, numEntries);
-        }
-
-        [TestMethod]
         public void TestSerializeRestaurants()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod]
-        public void TestSerializeReviews()
         {
             Assert.Fail();
         }
